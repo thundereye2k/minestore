6eac943a110310ad9f90ad86950c4418ec553278
ThatTonybo <twatt74@eq.edu.au>
2018-09-08 06:31:21 +1000

    New files, yay

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require('express')();
-const port = app.set('port', require('./config.json').port || 3000);
+app.set('port', require('./config.json').port || 3000);
 require('./router')(app);
 
+const port = app.get('port') || 3000;
 app.set('view engine', 'ejs');
 app.use(require('helmet')());
 app.use(require('express').static('assets'))

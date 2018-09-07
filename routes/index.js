2a3c8557732692570edc5b0880544d53d7d22faf
ThatTonybo <twatt74@eq.edu.au>
2018-09-08 06:40:54 +1000

    Major directory change to try fix bugs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
     res.status(200).render('index', {
         store: res.locals.store,
+        pageTitle: 'Welcome',
     });
 });
 
--- server.js
+++ server.js
@@ -5,6 +5,5 @@ require('./router')(app);
 const port = app.get('port') || 3000;
 app.set('view engine', 'ejs');
 app.use(require('helmet')());
-app.use(require('express').static('assets'))
 
 app.listen(port, () => console.info(`Listeing on port ${port}`));

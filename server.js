623aaea0753e27bbe10bce9f117dd07812786647
ThatTonybo <twatt74@eq.edu.au>
2018-09-08 06:47:43 +1000

    changes

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ require('./router')(app);
 const port = app.get('port') || 3000;
 app.set('view engine', 'ejs');
 app.use(require('helmet')());
+let path = require('path').join(__dirname + '/assets'); console.log(path)
+app.use(require('express').static(path));
 
 app.listen(port, () => console.info(`Listeing on port ${port}`));

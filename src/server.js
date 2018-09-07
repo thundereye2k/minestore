c901450fc98974565e16b6bd8c5565168172ef1d
ThatTonybo <twatt74@eq.edu.au>
2018-09-08 06:09:35 +1000

    Added helmet, package-lock and updated server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require('express')();
 const port = app.set('port', require('./config.json').port || 3000);
 
-
+app.set('view engine', 'ejs');
+app.use(require('helmet')());
+app.use(require('express').static('assets'))
 
 app.listen(port, () => console.info(`Listeing on port ${port}`));
